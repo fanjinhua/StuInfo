c11a3b845696f12288962c69af15c9d12384bfee
fanjinhua <fanyiyia@gmail.com>
2016-03-16 07:53:37 +0800

    set ui

--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@ from tkinter import *
 from database import *
 
 root = Tk()
-root.geometry('300x230')
+root.geometry('300x250')
 root.title('Database Modifier')
 
 frame = Frame(root, height=6, width=280)
@@ -37,7 +37,7 @@ def insert():
                 (e1.get(), e2.get(), e3.get(), e4.get(), e5.get()))
 
 def delete():
-    cur.execute('DE')
+    cur.execute('DELETE FROM stu_info WHERE 学号=?', [e1.get()])
 
 def find():
     cur.execute('SELECT * FROM stu_info WHERE 学号=?', [e1.get()])
@@ -49,6 +49,15 @@ def find():
     zy.set(value[0][3])
     xy.set(value[0][4])
 
+def clear():
+    num.set('')
+    name.set('')
+    sex.set('')
+    zy.set('')
+    xy.set('')
+
+#def save():
+ #   cur.execute('')
 
 #text = Text(root, height=6, width=430)
 
@@ -60,11 +69,13 @@ insert_button = Button(root, text='Insert', activeforeground='white', activeback
 delete_button = Button(root, text='Delete', activeforeground='white', activebackground='red', fg='red')
 find_button = Button(root, text='Find', activeforeground='white', activebackground='green', command=find)
 save_button = Button(root, text='Save', activeforeground='white', activebackground='green')
-insert_button.bind("<Button-1>")
+clear_button = Button(root, text='Clear', activeforeground='white', activebackground='green', command=clear)
+#insert_button.bind("<Button-1>")
 insert_button.pack(fill=X, expand=True)
 delete_button.pack(fill=X, expand=True)
 find_button.pack(fill=X, expand=True)
 save_button.pack(fill=X, expand=True)
+clear_button.pack(fill=X, expand=True)
 
 mainloop()
 
